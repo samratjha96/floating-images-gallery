3e4bb01ee3b4edd7811657a40dc684822a463a4d
Samrat Jha <samratj@amazon.com>
2023-08-05 18:55:35 -0400

    Basic floating images gallery

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,68 @@
+'use client';
+import { floating1, floating2, floating3, floating4, floating5, floating6, floating7, floating8 } from '@/data';
 import Image from 'next/image'
+import { useRef } from 'react'
+import gsap from 'gsap'
 
 export default function Home() {
+  const plane1 = useRef(null);
+  const plane2 = useRef(null);
+  const plane3 = useRef(null);
+  let requestAnimationFrameId = null;
+  let xForce = 0;
+  let yForce = 0;
+  const easing = 0.08;
+  const speed = 0.01;
+
+  const manageMouseMove = (e) => {
+    const { movementX, movementY } = e;
+    xForce += movementX * speed;
+    yForce += movementY * speed;
+
+    if(requestAnimationFrameId == null) {
+      requestAnimationFrameId = requestAnimationFrame(animate)
+    }
+  }
+
+  const lerp = (start, target, amount) => start * (1 - amount) +target * amount;
+
+  const animate = () => {
+    xForce = lerp(xForce, 0, easing);
+    yForce = lerp(yForce, 0, easing);
+    gsap.set(plane1.current, { x: `+=${xForce}`, y: `+=${yForce}`})
+    gsap.set(plane2.current, { x: `+=${xForce} * 0.5`, y: `+=${yForce} * 0.5`})
+    gsap.set(plane3.current, { x: `+=${xForce} * 0.25`, y: `+=${yForce} * 0.25`})
+
+    if(Math.abs(xForce) < 0.01) xForce = 0;
+    if(Math.abs(yForce) < 0.01) yForce = 0;
+
+    if(xForce !== 0 || yForce !== 0) {
+      requestAnimationFrame(animate);
+    } else {
+      cancelAnimationFrame(requestAnimationFrameId)
+      requestAnimationFrameId = null;
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex min-h-screen w-screen overflow-hidden relative" onMouseMove={(e) => {manageMouseMove(e)}}>
+      <div ref={plane1} className='absolute w-full h-full brightness-75'>
+        <Image src={floating1} alt='image' width={300} className='absolute left-[90%] top-[70%]'/>
+        <Image src={floating2} alt='image' width={300} className='absolute left-[5%] top-[65%]'/>
+        <Image src={floating7} alt='image' width={225} className='absolute left-[35%] top-[0%]'/>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div ref={plane2} className='absolute w-full h-full brightness-[0.6]'>
+        <Image src={floating4} alt='image' width={250} className='absolute left-[5%] top-[10%]'/>
+        <Image src={floating6} alt='image' width={200} className='absolute left-[80%] top-[5%]'/>
+        <Image src={floating8} alt='image' width={225} className='absolute left-[60%] top-[60%]'/>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div ref={plane3} className='absolute w-full h-full brightness-50'>
+        <Image src={floating3} alt='image' width={150} className='absolute left-[65%] top-[2.5%]'/>
+        <Image src={floating5} alt='image' width={200} className='absolute left-[40%] top-[75%]'/>
+      </div>
+      <div className='absolute left-1/2 top-[45%] -translate-x-1/2 -translate-y-1/2'>
+        <h1 className='text-xl text-white-500 text-center m-0 font-normal'>Floating Images Gallery</h1>
+        <p className='text-gray-500 m-0 text-center mt-[10px]'>Next.js and GSAP</p>
       </div>
     </main>
   )
---
+++ src/data.js
@@ -0,0 +1,19 @@
+import floating1 from '../public/images/floating_1.jpeg'
+import floating2 from '../public/images/floating_2.jpeg'
+import floating3 from '../public/images/floating_3.jpeg'
+import floating4 from '../public/images/floating_4.jpeg'
+import floating5 from '../public/images/floating_5.jpeg'
+import floating6 from '../public/images/floating_6.jpeg'
+import floating7 from '../public/images/floating_7.jpeg'
+import floating8 from '../public/images/floating_8.jpeg'
+
+export {
+    floating1,
+    floating2,
+    floating3,
+    floating4,
+    floating5,
+    floating6,
+    floating7,
+    floating8
+}
